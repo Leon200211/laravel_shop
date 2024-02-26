951ad676eeabaea793116d1cbcd7a5f6695ca329
Leon200211 <leon200207@yandex.ru>
2024-02-26 23:08:03 +0300

    Таблицы главной страницы

--- laravel-shop/database/seeders/DatabaseSeeder.php
+++ laravel-shop/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +15,7 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         // \App\Models\User::factory(10)->create();
 
@@ -20,5 +23,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Brand::factory(20)->create();
+        Product::factory(20)
+            ->has(Category::factory(rand(1, 3)))
+            ->create()
+        ;
     }
 }
