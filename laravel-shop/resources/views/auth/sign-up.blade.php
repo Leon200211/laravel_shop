3f0d2dc15cea07f4721c4c3843ee29a058a0766a
Leon200211 <leon200207@yandex.ru>
2024-07-24 13:17:20 +0300

    Авторизация
    Верстка

--- laravel-shop/resources/views/auth/sign-up.blade.php
+++ laravel-shop/resources/views/auth/sign-up.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.auth')
 
-@section('title', 'Вход в аккаунт')
+@section('title', 'Регистрация')
 
 @section('content')
 
-    <x-forms.auth-forms title="Вход в аккаунт" action="">
+    <x-forms.auth-forms title="Регистрация" action="">
         @csrf
+
+        <x-forms.text-input
+            name="name"
+            placeholder="ФИО"
+            :isError="$errors->has('name')"
+            required
+        />
+        @error('name')
+        <x-forms.error>
+            {{ $message }}
+        </x-forms.error>
+        @enderror
 
         <x-forms.text-input
             name="email"
@@ -27,9 +39,27 @@
             :isError="$errors->has('password')"
             required
         />
+        @error('password')
+        <x-forms.error>
+            {{ $message }}
+        </x-forms.error>
+        @enderror
+
+        <x-forms.text-input
+            name="password_confirmation"
+            type="password"
+            placeholder="Подтверждение пароля"
+            :isError="$errors->has('password_confirmation')"
+            required
+        />
+        @error('password_confirmation')
+        <x-forms.error>
+            {{ $message }}
+        </x-forms.error>
+        @enderror
 
         <x-forms.primary-button>
-            Войти
+            Зарегистрироваться
         </x-forms.primary-button>
 
         <x-slot:socialAuth>
@@ -47,8 +77,7 @@
 
         <x-slot:buttons>
             <div class="space-y-3 mt-5">
-                <div class="text-xxs md:text-xs"><a href="#" class="text-white hover:text-white/70 font-bold">Забыли пароль?</a></div>
-                <div class="text-xxs md:text-xs"><a href="{{ route('login') }}" class="text-white hover:text-white/70 font-bold">Войти в аккаунт</a></div>
+                <div class="text-xxs md:text-xs"><a href="#" class="text-white hover:text-white/70 font-bold">Регистрация</a></div>
             </div>
         </x-slot:buttons>
     </x-forms.auth-forms>
