12dc1066508224bbf9331463e096a93a4a1c04b9
Leon200211 <leon200207@yandex.ru>
2024-08-04 16:56:50 +0300

    Тестирование
    Добавил тест отправки запроса в апи телеграмма, добавил отлов отправки реальных запросов в тестах

---
+++ laravel-shop/tests/Unit/Services/Telegram/TelegramBotApiTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Services\Telegram;
+
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class TelegramBotApiTest extends TestCase
+{
+
+    /**
+     * @test Тестирует отправку данных в api телеграмма
+     *
+     * @return void
+     */
+    public function it_send_message_success(): void
+    {
+        Http::fake([
+            TelegramBotApi::HOST . '*' => Http::response(['ok' => true]),
+        ]);
+
+        $result = TelegramBotApi::sendMessage('', 1, 'Testing');
+
+        $this->assertTrue($result);
+    }
+}
