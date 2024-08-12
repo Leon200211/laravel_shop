e5408c9a73dd4e42a26a069ad29f8f75ad5047a4
Leon200211 <leon200207@yandex.ru>
2024-08-12 11:53:01 +0300

    Кэширование
    Добавил кэширования запроса категорий к БД для главной страницы

--- laravel-shop/src/Domain/Catalog/ViewModels/CategoryViewModel.php
+++ laravel-shop/src/Domain/Catalog/ViewModels/CategoryViewModel.php
@@ -4,6 +4,7 @@ namespace Domain\Catalog\ViewModels;
 
 use Domain\Catalog\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
 use Support\Traits\Makeable;
 
 final class CategoryViewModel
@@ -12,6 +13,10 @@ final class CategoryViewModel
 
     public function homePage(): Collection|array
     {
-        return Category::query()->homePage()->get();
+        // TODO добавить обсервер на обновление категирий чтобы сбросить кэш по ключу
+        // tags(['category']) если переходить на нормальное кэширование
+        return Cache::rememberForever('category_home_page', function () {
+            return Category::query()->homePage()->get();
+        });
     }
 }
