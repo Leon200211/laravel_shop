f6a5052d9eac59a34d12a3d1173ff8254b6502c3
Leon200211 <leon200207@yandex.ru>
2024-08-18 19:45:47 +0300

    Отображение товаров в каталоге
    Добавил возможность менять вид отображения. Сделал это через миделвар

--- laravel-shop/resources/views/catalog/index.blade.php
+++ laravel-shop/resources/views/catalog/index.blade.php
@@ -60,12 +60,12 @@
                 <div class="flex flex-col md:flex-row md:items-center justify-between gap-4 mb-8">
                     <div class="flex items-center gap-4">
                         <div class="flex items-center gap-2">
-                            <a href="#" class="pointer-events-none inline-flex items-center justify-center w-10 h-10 rounded-md bg-card text-pink">
+                            <a href="{{ filter_url($category, ['view' => 'grid']) }}" class="@if(is_catalog_view('grid')) text-pink pointer-events-none @else text-white @endif inline-flex items-center justify-center w-10 h-10 rounded-md bg-card hover:text-pink">
                                 <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 52 52">
                                     <path fill-rule="evenodd" d="M2.6 28.6h18.2a2.6 2.6 0 0 1 2.6 2.6v18.2a2.6 2.6 0 0 1-2.6 2.6H2.6A2.6 2.6 0 0 1 0 49.4V31.2a2.6 2.6 0 0 1 2.6-2.6Zm15.6 18.2v-13h-13v13h13ZM31.2 0h18.2A2.6 2.6 0 0 1 52 2.6v18.2a2.6 2.6 0 0 1-2.6 2.6H31.2a2.6 2.6 0 0 1-2.6-2.6V2.6A2.6 2.6 0 0 1 31.2 0Zm15.6 18.2v-13h-13v13h13ZM31.2 28.6h18.2a2.6 2.6 0 0 1 2.6 2.6v18.2a2.6 2.6 0 0 1-2.6 2.6H31.2a2.6 2.6 0 0 1-2.6-2.6V31.2a2.6 2.6 0 0 1 2.6-2.6Zm15.6 18.2v-13h-13v13h13ZM2.6 0h18.2a2.6 2.6 0 0 1 2.6 2.6v18.2a2.6 2.6 0 0 1-2.6 2.6H2.6A2.6 2.6 0 0 1 0 20.8V2.6A2.6 2.6 0 0 1 2.6 0Zm15.6 18.2v-13h-13v13h13Z" clip-rule="evenodd"/>
                                 </svg>
                             </a>
-                            <a href="#" class="inline-flex items-center justify-center w-10 h-10 rounded-md bg-card text-white hover:text-pink">
+                            <a href="{{ filter_url($category, ['view' => 'list']) }}" class="@if(is_catalog_view('list')) text-pink pointer-events-none @else text-white @endif inline-flex items-center justify-center w-10 h-10 rounded-md bg-card  hover:text-pink">
                                 <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 52 52">
                                     <path fill-rule="evenodd" d="M7.224 4.875v4.694h37.555V4.875H7.224ZM4.877.181a2.347 2.347 0 0 0-2.348 2.347v9.389a2.347 2.347 0 0 0 2.348 2.347h42.25a2.347 2.347 0 0 0 2.347-2.347v-9.39A2.347 2.347 0 0 0 47.127.182H4.877Zm2.347 23.472v4.694h37.555v-4.694H7.224Zm-2.347-4.695a2.347 2.347 0 0 0-2.348 2.348v9.389a2.347 2.347 0 0 0 2.348 2.347h42.25a2.347 2.347 0 0 0 2.347-2.348v-9.388a2.347 2.347 0 0 0-2.347-2.348H4.877ZM7.224 42.43v4.695h37.555v-4.694H7.224Zm-2.347-4.694a2.347 2.347 0 0 0-2.348 2.347v9.39a2.347 2.347 0 0 0 2.348 2.346h42.25a2.347 2.347 0 0 0 2.347-2.347v-9.389a2.347 2.347 0 0 0-2.347-2.347H4.877Z" clip-rule="evenodd"/>
                                 </svg>
@@ -94,8 +94,8 @@
                 </div>
 
 
-                <div class="products grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 2xl:gap-x-8 gap-y-8 lg:gap-y-10 2xl:gap-y-12">
-                    @each('catalog.shared.products', $products, 'item')
+                <div class="products grid @if(is_catalog_view('list')) grid-cols-1 gap-y-8 @else grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 2xl:gap-x-8 gap-y-8 lg:gap-y-10 2xl:gap-y-12 @endif">
+                    @each('product.shared.product' . (is_catalog_view('list') ? '-list' : ''), $products, 'item')
                 </div>
 
 
--- laravel-shop/src/Support/helpers.php
+++ laravel-shop/src/Support/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Domain\Catalog\Filters\FilterManager;
+use Domain\Catalog\Models\Category;
 use Support\Flash\Flash;
 
 if (!function_exists('flash')) {
@@ -14,5 +15,23 @@ if (!function_exists('filters')) {
     function filters(): array
     {
         return app(FilterManager::class)->items();
+    }
+}
+
+if (!function_exists('is_catalog_view')) {
+    function is_catalog_view(string $type, string $default = 'grid'): bool
+    {
+        return session('view', $default) === $type;
+    }
+}
+
+if (!function_exists('filter_url')) {
+    function filter_url(?Category $category, array $params = []): string
+    {
+        return route('catalog', [
+            ...request()->only(['filters', 'sort']),
+            ...$params,
+            'category' => $category
+        ]);
     }
 }
