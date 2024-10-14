123fa0c9ecba63193264a1d19b64a94de568c92c
Leon200211 <leon200207@yandex.ru>
2024-10-14 21:43:03 +0300

    Оплата
    Добавил функционал для внедрения оплаты

--- laravel-shop/src/Domain/Order/States/Payment/PaymentState.php
+++ laravel-shop/src/Domain/Order/States/Payment/PaymentState.php
@@ -1,8 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Domain\Order\States\Payment;
 
-class PaymentState
-{
+use Spatie\ModelStates\State;
+use Spatie\ModelStates\StateConfig;
 
+abstract class PaymentState extends State
+{
+    public static function config(): StateConfig
+    {
+        return parent::config()
+            ->default(PendingPaymentState::class)
+            ->allowTransition(PendingPaymentState::class, PaidPaymentState::class)
+            ->allowTransition(PendingPaymentState::class, CancelledPaymentState::class)
+        ;
+    }
 }
